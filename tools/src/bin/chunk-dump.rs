5e33f4289d8ca8b7857ed9b66eaf6cd4e9c6c68f
Owen Gage <owengage@gmail.com>
2020-09-30 10:35:03 +0100

    Fix chunk-dump

--- tools/src/bin/chunk-dump.rs
+++ tools/src/bin/chunk-dump.rs
@@ -1,7 +1,5 @@
 use fastnbt::anvil;
-use fastnbt::nbt;
 use flate2::read::ZlibDecoder;
-use std::io;
 use std::io::Read;
 
 use fastnbt::anvil::types::Chunk;
@@ -9,15 +7,13 @@ use fastnbt::nbt2::de::from_bytes;
 
 fn main() {
     let args: Vec<_> = std::env::args().skip(1).collect();
-    //let file = std::fs::File::open(args[0].clone()).unwrap();
-    let file =
-        std::fs::File::open("/home/ogage/.minecraft/saves/New World/region/r.0.0.mca").unwrap();
+    let file = std::fs::File::open(args[0].clone()).unwrap();
 
     let mut region = anvil::Region::new(file);
     let loc = region.chunk_location(0, 0).unwrap();
 
     let mut chunk_data = vec![0u8; loc.sector_count * anvil::SECTOR_SIZE];
-    region.load_chunk(&loc, chunk_data.as_mut_slice()).unwrap();
+    region.load_chunk(&loc, &mut chunk_data).unwrap();
 
     let mut decoder = ZlibDecoder::new(&chunk_data[5..]);
     let mut data = vec![];
