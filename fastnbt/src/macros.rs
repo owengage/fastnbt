ba0107fbdf54569dcb48d66707e27354f1e39426
Owen Gage <owengage@gmail.com>
2022-04-24 09:57:12 +0100

    Change nbt! use to nbt_internal! where appropriate

--- fastnbt/src/macros.rs
+++ fastnbt/src/macros.rs
@@ -79,27 +79,27 @@ macro_rules! nbt_internal {
 
     // Next element is an array.
     (@array [$($elems:expr,)*] [$($array:tt)*] $($rest:tt)*) => {
-        nbt!(@array [$($elems,)* nbt!([$($array)*])] $($rest)*)
+        nbt_internal!(@array [$($elems,)* nbt_internal!([$($array)*])] $($rest)*)
     };
 
     // Next element is a map.
     (@array [$($elems:expr,)*] {$($map:tt)*} $($rest:tt)*) => {
-        nbt!(@array [$($elems,)* nbt!({$($map)*})] $($rest)*)
+        nbt_internal!(@array [$($elems,)* nbt_internal!({$($map)*})] $($rest)*)
     };
 
     // Next element is an expression followed by comma.
     (@array [$($elems:expr,)*] $next:expr, $($rest:tt)*) => {
-        nbt!(@array [$($elems,)* nbt!($next),] $($rest)*)
+        nbt_internal!(@array [$($elems,)* nbt_internal!($next),] $($rest)*)
     };
 
     // Last element is an expression with no trailing comma.
     (@array [$($elems:expr,)*] $last:expr) => {
-        nbt!(@array [$($elems,)* nbt!($last)])
+        nbt_internal!(@array [$($elems,)* nbt_internal!($last)])
     };
 
     // Comma after the most recent element.
     (@array [$($elems:expr),*] , $($rest:tt)*) => {
-        nbt!(@array [$($elems,)*] $($rest)*)
+        nbt_internal!(@array [$($elems,)*] $($rest)*)
     };
 
     // Unexpected token after most recent element.
@@ -121,17 +121,17 @@ macro_rules! nbt_internal {
 
     // Next element is an expression followed by comma.
     (@int_array [$($elems:expr,)*] $next:expr, $($rest:tt)*) => {
-        nbt!(@int_array [$($elems,)* $next,] $($rest)*)
+        nbt_internal!(@int_array [$($elems,)* $next,] $($rest)*)
     };
 
     // Last element is an expression with no trailing comma.
     (@int_array [$($elems:expr,)*] $last:expr) => {
-        nbt!(@int_array [$($elems,)* $last])
+        nbt_internal!(@int_array [$($elems,)* $last])
     };
 
     // Comma after the most recent element.
     (@int_array [$($elems:expr),*] , $($rest:tt)*) => {
-        nbt!(@int_array [$($elems,)*] $($rest)*)
+        nbt_internal!(@int_array [$($elems,)*] $($rest)*)
     };
 
     // Unexpected token after most recent element.
@@ -143,7 +143,7 @@ macro_rules! nbt_internal {
     // TT muncher for parsing the inside of an object {...}. Each entry is
     // inserted into the given map variable.
     //
-    // Must be invoked as: nbt!(@object $map () ($($tt)*) ($($tt)*))
+    // Must be invoked as: nbt_internal!(@object $map () ($($tt)*) ($($tt)*))
     //
     // We require two copies of the input tokens so that we can match on one
     // copy and trigger errors on the other copy.
@@ -155,7 +155,7 @@ macro_rules! nbt_internal {
     // Insert the current entry followed by trailing comma.
     (@object $object:ident [$($key:tt)+] ($value:expr) , $($rest:tt)*) => {
         let _ = $object.insert(($($key)+).into(), $value);
-        nbt!(@object $object () ($($rest)*) ($($rest)*));
+        nbt_internal!(@object $object () ($($rest)*) ($($rest)*));
     };
 
     // Current entry followed by unexpected token.
@@ -170,35 +170,35 @@ macro_rules! nbt_internal {
 
     // Next value is an array.
     (@object $object:ident ($($key:tt)+) (: [$($array:tt)*] $($rest:tt)*) $copy:tt) => {
-        nbt!(@object $object [$($key)+] (nbt!([$($array)*])) $($rest)*);
+        nbt_internal!(@object $object [$($key)+] (nbt_internal!([$($array)*])) $($rest)*);
     };
 
     // Next value is a map.
     (@object $object:ident ($($key:tt)+) (: {$($map:tt)*} $($rest:tt)*) $copy:tt) => {
-        nbt!(@object $object [$($key)+] (nbt!({$($map)*})) $($rest)*);
+        nbt_internal!(@object $object [$($key)+] (nbt_internal!({$($map)*})) $($rest)*);
     };
 
     // Next value is an expression followed by comma.
     (@object $object:ident ($($key:tt)+) (: $value:expr , $($rest:tt)*) $copy:tt) => {
-        nbt!(@object $object [$($key)+] (nbt!($value)) , $($rest)*);
+        nbt_internal!(@object $object [$($key)+] (nbt_internal!($value)) , $($rest)*);
     };
 
     // Last value is an expression with no trailing comma.
     (@object $object:ident ($($key:tt)+) (: $value:expr) $copy:tt) => {
-        nbt!(@object $object [$($key)+] (nbt!($value)));
+        nbt_internal!(@object $object [$($key)+] (nbt_internal!($value)));
     };
 
     // Missing value for last entry. Trigger a reasonable error message.
     (@object $object:ident ($($key:tt)+) (:) $copy:tt) => {
         // "unexpected end of macro invocation"
-        nbt!();
+        nbt_internal!();
     };
 
     // Missing colon and value for last entry. Trigger a reasonable error
     // message.
     (@object $object:ident ($($key:tt)+) () $copy:tt) => {
         // "unexpected end of macro invocation"
-        nbt!();
+        nbt_internal!();
     };
 
     // Misplaced colon. Trigger a reasonable error message.
@@ -216,7 +216,7 @@ macro_rules! nbt_internal {
     // Key is fully parenthesized. This avoids clippy double_parens false
     // positives because the parenthesization may be necessary here.
     (@object $object:ident () (($key:expr) : $($rest:tt)*) $copy:tt) => {
-        nbt!(@object $object ($key) (: $($rest)*) (: $($rest)*));
+        nbt_internal!(@object $object ($key) (: $($rest)*) (: $($rest)*));
     };
 
     // Refuse to absorb colon token into key expression.
@@ -226,13 +226,13 @@ macro_rules! nbt_internal {
 
     // Munch a token into the current key.
     (@object $object:ident ($($key:tt)*) ($tt:tt $($rest:tt)*) $copy:tt) => {
-        nbt!(@object $object ($($key)* $tt) ($($rest)*) ($($rest)*));
+        nbt_internal!(@object $object ($($key)* $tt) ($($rest)*) ($($rest)*));
     };
 
     //////////////////////////////////////////////////////////////////////////
     // The main implementation.
     //
-    // Must be invoked as: nbt!($($json)+)
+    // Must be invoked as: nbt_internal!($($nbt)+)
     //////////////////////////////////////////////////////////////////////////
 
     ([B;]) => {
@@ -252,19 +252,19 @@ macro_rules! nbt_internal {
     };
 
     ([B; $($tt:tt)+ ]) => {
-        $crate::Value::ByteArray($crate::ByteArray::new(nbt!(@int_array [] $($tt)+)))
+        $crate::Value::ByteArray($crate::ByteArray::new(nbt_internal!(@int_array [] $($tt)+)))
     };
 
     ([I; $($tt:tt)+ ]) => {
-        $crate::Value::IntArray($crate::IntArray::new(nbt!(@int_array [] $($tt)+)))
+        $crate::Value::IntArray($crate::IntArray::new(nbt_internal!(@int_array [] $($tt)+)))
     };
 
     ([L; $($tt:tt)+ ]) => {
-        $crate::Value::LongArray($crate::LongArray::new(nbt!(@int_array [] $($tt)+)))
+        $crate::Value::LongArray($crate::LongArray::new(nbt_internal!(@int_array [] $($tt)+)))
     };
 
     ([ $($tt:tt)+ ]) => {
-        $crate::Value::List(nbt!(@array [] $($tt)+))
+        $crate::Value::List(nbt_internal!(@array [] $($tt)+))
     };
 
     ({}) => {
@@ -274,7 +274,7 @@ macro_rules! nbt_internal {
     ({ $($tt:tt)+ }) => {
         $crate::Value::Compound({
             let mut object = std::collections::HashMap::new();
-            nbt!(@object object () ($($tt)+) ($($tt)+));
+            nbt_internal!(@object object () ($($tt)+) ($($tt)+));
             object
         })
     };
@@ -297,7 +297,7 @@ macro_rules! nbt_expect_expr_comma {
     ($e:expr , $($tt:tt)*) => {};
 }
 
-// The json_internal macro above cannot invoke vec directly because it uses
+// The nbt_internal macro above cannot invoke vec directly because it uses
 // local_inner_macros. A vec invocation there would resolve to $crate::vec.
 // Instead invoke vec here outside of local_inner_macros.
 #[macro_export]
