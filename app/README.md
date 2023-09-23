9190733315bd1e672bafb5234fc91ef4d1d7ae43
Owen Gage <owengage@gmail.com>
2023-09-23 10:46:13 +0100

    Remove anvil app to own repository.

--- app/README.md
+++
@@ -1,4 +0,0 @@
-# Anvil App
-
-Prototype Anvil desktop application. See [Tauri](https://tauri.app/)
-documentation for building and running.
--- app/src/Ribbon.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactFragment } from "react";
-import "./Ribbon.css";
-
-export interface RinnonPropsInner {}
-type RibbonProps = React.PropsWithChildren<RinnonPropsInner>;
-
-export function Ribbon({ children }: RibbonProps) {
-  return <div className="ribbon">{children}</div>;
-}
