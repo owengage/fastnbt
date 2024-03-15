a7127e9a71700afc1e5f29490684dd57c21a56f7
hollowness-inside-pc <malikov.vp@students.dvfu.ru>
2024-03-15 11:01:08 +1000

    Reformat and allow trailing comma

--- fastnbt/src/ser/array_serializer.rs
+++ fastnbt/src/ser/array_serializer.rs
@@ -14,20 +14,20 @@ pub(crate) struct ArraySerializer<'a, W: Write> {
 }
 
 macro_rules! only_bytes {
-    {$v:ident($($t:ty),*) -> $r:ty} => {
-        fn $v(self, $(_: $t,)*) -> Result<$r> {
+    ($v:ident($($t:ty),* $(,)?) -> $r:ty) => {
+        fn $v(self, $(_: $t),*) -> Result<$r> {
             Err(Error::array_as_other())
         }
     };
 
-    {$v:ident<T>($($t:ty),*)} => {
-        fn $v<T: ?Sized>(self, $(_: $t,)*) -> Result<Self::Ok> {
+    ($v:ident<T>($($t:ty),* $(,)?)) => {
+        fn $v<T: ?Sized>(self, $(_: $t),*) -> Result<Self::Ok> {
             Err(Error::array_as_other())
         }
     };
 
-    {$v:ident($($t:ty),*)} => {
-        only_bytes!{$v($($t),*) -> ()}
+    ($v:ident($($t:ty),* $(,)?)) => {
+        only_bytes!{$v($($t,)*) -> ()}
     };
 }
 
@@ -55,33 +55,49 @@ impl<'a, W: Write> serde::Serializer for ArraySerializer<'a, W> {
         Ok(())
     }
 
-    only_bytes! {serialize_bool(bool)}
-    only_bytes! {serialize_i8(i8)}
-    only_bytes! {serialize_i16(i16)}
-    only_bytes! {serialize_i32(i32)}
-    only_bytes! {serialize_i64(i64)}
-    only_bytes! {serialize_i128(i128)}
-    only_bytes! {serialize_u8(u8)}
-    only_bytes! {serialize_u16(u16)}
-    only_bytes! {serialize_u32(u32)}
-    only_bytes! {serialize_u64(u64)}
-    only_bytes! {serialize_u128(u128)}
-    only_bytes! {serialize_f32(f32)}
-    only_bytes! {serialize_f64(f64)}
-    only_bytes! {serialize_char(char)}
-    only_bytes! {serialize_str(&str)}
-    only_bytes! {serialize_none()}
-    only_bytes! {serialize_some<T>(&T)}
-    only_bytes! {serialize_unit()}
-    only_bytes! {serialize_unit_struct(&'static str)}
-    only_bytes! {serialize_unit_variant(&'static str, u32, &'static str) -> Self::Ok}
-    only_bytes! {serialize_newtype_struct<T>(&'static str, &T)}
-    only_bytes! {serialize_newtype_variant<T>(&'static str, u32, &'static str, &T)}
-    only_bytes! {serialize_seq(Option<usize>) -> Self::SerializeSeq}
-    only_bytes! {serialize_tuple(usize) -> Self::SerializeSeq}
-    only_bytes! {serialize_tuple_variant(&'static str, u32, &'static str, usize) -> Self::SerializeTupleVariant}
-    only_bytes! {serialize_map(Option<usize>) -> Self::SerializeMap}
-    only_bytes! {serialize_tuple_struct(&'static str, usize) -> Self::SerializeTupleStruct}
-    only_bytes! {serialize_struct(&'static str, usize) -> Self::SerializeStruct}
-    only_bytes! {serialize_struct_variant(&'static str, u32, &'static str, usize) -> Self::SerializeStructVariant}
+    only_bytes!(serialize_bool(bool));
+    only_bytes!(serialize_i8(i8));
+    only_bytes!(serialize_i16(i16));
+    only_bytes!(serialize_i32(i32));
+    only_bytes!(serialize_i64(i64));
+    only_bytes!(serialize_i128(i128));
+    only_bytes!(serialize_u8(u8));
+    only_bytes!(serialize_u16(u16));
+    only_bytes!(serialize_u32(u32));
+    only_bytes!(serialize_u64(u64));
+    only_bytes!(serialize_u128(u128));
+    only_bytes!(serialize_f32(f32));
+    only_bytes!(serialize_f64(f64));
+    only_bytes!(serialize_char(char));
+    only_bytes!(serialize_str(&str));
+    only_bytes!(serialize_none());
+    only_bytes!(serialize_some<T>(&T));
+    only_bytes!(serialize_unit());
+    only_bytes!(serialize_unit_struct(&'static str));
+    only_bytes!(serialize_unit_variant(&'static str, u32, &'static str) -> Self::Ok);
+    only_bytes!(serialize_newtype_struct<T>(&'static str, &T));
+    only_bytes!(serialize_newtype_variant<T>(&'static str, u32, &'static str, &T));
+    only_bytes!(serialize_seq(Option<usize>) -> Self::SerializeSeq);
+    only_bytes!(serialize_tuple(usize) -> Self::SerializeSeq);
+    only_bytes!(serialize_map(Option<usize>) -> Self::SerializeMap);
+    only_bytes!(serialize_tuple_struct(&'static str, usize) -> Self::SerializeTupleStruct);
+    only_bytes!(serialize_struct(&'static str, usize) -> Self::SerializeStruct);
+
+    only_bytes!(
+        serialize_tuple_variant(
+            &'static str,
+            u32,
+            &'static str,
+            usize,
+        ) -> Self::SerializeTupleVariant
+    );
+
+    only_bytes!(
+        serialize_struct_variant(
+            &'static str,
+            u32,
+            &'static str,
+            usize,
+        ) -> Self::SerializeStructVariant
+    );
 }
